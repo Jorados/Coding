9efc8a50a9f7b7ca92386b97f713abbe9b7232ff
Seongjin Jo <seongjin8860@naver.com>
2023-01-18 13:43:44 +0900

    DFS,BFS 기초 예제

--- src/Recursive_Tree_Graph/ex2.java
+++ src/Recursive_Tree_Graph/ex2.java
@@ -2,17 +2,18 @@ package Recursive_Tree_Graph;
 
 import java.util.Scanner;
 
-//재귀를 이용한 이진수 출력
 public class ex2 {
-    public static void solution(String n){
-        int two = Integer.parseInt(n, 2);
-        System.out.println(two);
+    public static void DFS(int n){
+        if(n==0) return;
+        else{
+            DFS(n/2);
+            System.out.print(n%2);
+        }
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String n = sc.next();
-        solution(n);
+        int n = sc.nextInt();
+        DFS(n);
     }
-
 }
--- src/Recursive_Tree_Graph/ex5.java
+++ src/Recursive_Tree_Graph/ex5.java
@@ -1,20 +1,18 @@
 package Recursive_Tree_Graph;
 
-import java.util.Scanner;
-
 //DFS 이진트리 순회
 //전위 중위 후위
 class Node{
     int data;
-    Node lt,rt; //node객체의 주소를 저장하는 변수
+    Node4 lt,rt; //node객체의 주소를 저장하는 변수
     public Node(int val){
         data=val;
         lt=rt=null;
     }
 }
 public class ex5 {
-    Node root;
-    public void DFS(Node root){
+    Node4 root;
+    public void DFS(Node4 root){
         if(root==null) return;
         else{
             System.out.println(root.data + " ");
@@ -24,13 +22,13 @@ public class ex5 {
     }
     public static void main(String[] args) {
         ex5 tree = new ex5();
-        tree.root=new Node(1);
-        tree.root.lt=new Node(2);
-        tree.root.rt=new Node(3);
-        tree.root.lt.lt=new Node(4);
-        tree.root.lt.rt=new Node(5);
-        tree.root.rt.lt=new Node(6);
-        tree.root.rt.rt=new Node(7);
+        tree.root=new Node4(1);
+        tree.root.lt=new Node4(2);
+        tree.root.rt=new Node4(3);
+        tree.root.lt.lt=new Node4(4);
+        tree.root.lt.rt=new Node4(5);
+        tree.root.rt.lt=new Node4(6);
+        tree.root.rt.rt=new Node4(7);
         tree.DFS(tree.root);
 
     }
